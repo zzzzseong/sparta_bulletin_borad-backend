0d38bd665e54d144f6036e9066afb609b9023b21
zzzzseong <jung48182@gmail.com>
2023-11-03 13:06:34 +0900

    initial commit

---
+++ src/main/java/com/sparta/spartabulletinboardbackend/SpartaBulletinBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartabulletinboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaBulletinBoardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaBulletinBoardBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spartabulletinboardbackend/SpartaBulletinBoardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartabulletinboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaBulletinBoardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
