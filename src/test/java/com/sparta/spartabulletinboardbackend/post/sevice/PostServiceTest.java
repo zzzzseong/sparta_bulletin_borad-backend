70c7d955cfa10e76d4334b2f186ebad0650f7711
zzzzseong <jung48182@gmail.com>
2023-12-03 16:13:04 +0900

    test(post): TODO 생성 실패(제목 없음) 테스트 추가

--- src/test/java/com/sparta/spartabulletinboardbackend/post/sevice/PostServiceTest.java
+++ src/test/java/com/sparta/spartabulletinboardbackend/post/sevice/PostServiceTest.java
@@ -1,5 +1,7 @@
 package com.sparta.spartabulletinboardbackend.post.sevice;
 
+import com.sparta.spartabulletinboardbackend.common.exception.CustomErrorCode;
+import com.sparta.spartabulletinboardbackend.common.exception.CustomException;
 import com.sparta.spartabulletinboardbackend.post.dto.PostCreateRequest;
 import com.sparta.spartabulletinboardbackend.post.entity.Post;
 import com.sparta.spartabulletinboardbackend.post.repository.PostRepository;
@@ -13,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
@@ -45,10 +48,28 @@ class PostServiceTest {
         assertEquals(request.getContent(), post.getContent());
     }
 
-//    @Test
-//    @DisplayName("TODO 생성(실패) - 제목이 유효하지 않음")
-//    public void savePostTestTitleInvalid() {
-//
-//    }
+    @Test
+    @DisplayName("TODO 생성(실패) - 제목 없음")
+    public void savePostTestTitleInvalid() {
+        //given
+        User user = User.builder()
+                .username("username")
+                .email("email@email.com")
+                .password("passwordA1~")
+                .userRole(UserRole.USER)
+                .build();
+
+        PostCreateRequest request = new PostCreateRequest();
+        request.setTitle("");
+        request.setContent("content");
+
+        PostService postService = new PostService(postRepository);
+
+        //when
+        CustomException exception = assertThrows(CustomException.class, () -> postService.savePost(user, request));
+
+        //then
+        assertEquals(CustomErrorCode.POST_TITLE_INVALID_EXCEPTION, exception.getErrorCode());
+    }
 
 }
