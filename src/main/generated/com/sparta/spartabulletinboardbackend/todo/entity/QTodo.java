921a3a4fcdcd9da8fb54ad308d89a9e3c084bf28
zzzzseong <jung48182@gmail.com>
2023-12-22 18:30:44 +0900

    feat(querydsl): querydsl 사용을 위한 Q 객체 추가

---
+++ src/main/generated/com/sparta/spartabulletinboardbackend/todo/entity/QTodo.java
@@ -0,0 +1,71 @@
+package com.sparta.spartabulletinboardbackend.todo.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QTodo is a Querydsl query type for Todo
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QTodo extends EntityPathBase<Todo> {
+
+    private static final long serialVersionUID = 1408367521L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QTodo todo = new QTodo("todo");
+
+    public final com.sparta.spartabulletinboardbackend.common.QBaseEntity _super = new com.sparta.spartabulletinboardbackend.common.QBaseEntity(this);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> lastModifiedAt = _super.lastModifiedAt;
+
+    //inherited
+    public final StringPath lastModifiedBy = _super.lastModifiedBy;
+
+    public final BooleanPath success = createBoolean("success");
+
+    public final StringPath title = createString("title");
+
+    public final com.sparta.spartabulletinboardbackend.user.entity.QUser user;
+
+    public QTodo(String variable) {
+        this(Todo.class, forVariable(variable), INITS);
+    }
+
+    public QTodo(Path<? extends Todo> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QTodo(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QTodo(PathMetadata metadata, PathInits inits) {
+        this(Todo.class, metadata, inits);
+    }
+
+    public QTodo(Class<? extends Todo> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.user = inits.isInitialized("user") ? new com.sparta.spartabulletinboardbackend.user.entity.QUser(forProperty("user")) : null;
+    }
+
+}
+
---
+++ src/main/generated/com/sparta/spartabulletinboardbackend/user/entity/QUser.java
@@ -0,0 +1,61 @@
+package com.sparta.spartabulletinboardbackend.user.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = 1247339307L;
+
+    public static final QUser user = new QUser("user");
+
+    public final com.sparta.spartabulletinboardbackend.common.QBaseEntity _super = new com.sparta.spartabulletinboardbackend.common.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final NumberPath<Long> kakaoId = createNumber("kakaoId", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> lastModifiedAt = _super.lastModifiedAt;
+
+    //inherited
+    public final StringPath lastModifiedBy = _super.lastModifiedBy;
+
+    public final StringPath password = createString("password");
+
+    public final StringPath username = createString("username");
+
+    public final EnumPath<UserRole> userRole = createEnum("userRole", UserRole.class);
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
