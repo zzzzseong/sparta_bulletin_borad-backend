3db6324205161c130cef89c137e3b52b7d724e32
zzzzseong <jung48182@gmail.com>
2023-12-22 18:33:42 +0900

    refactor(config): JpaAuditingConfig 분리 - @EnableJpaAuditing으로 인해 테스트 과정에서 Bean 주입이 되지 않아 에러 발생 - 별도 config파일을 생성하며 해결 (common -> configuration -> JpaAuditingConfig.java

--- src/main/java/com/sparta/spartabulletinboardbackend/TODOBackendApplication.java
+++ src/main/java/com/sparta/spartabulletinboardbackend/TODOBackendApplication.java
@@ -2,9 +2,7 @@ package com.sparta.spartabulletinboardbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class TODOBackendApplication {
 
